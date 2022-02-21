90e2f61f390374097bf8d85a5b48951492079ef5
Marcin <marcin.cija@gmail.com>
2022-02-21 20:32:33 +0100

    update

--- src/components/Offer.js
+++ src/components/Offer.js
@@ -3,30 +3,49 @@ import Box from "./Box";
 
 function Offer() {
   const offers = [
-    "Usługa 1",
-    "Usługa 2",
-    "Usługa 3",
-    "Usługa 4",
-    "Usługa 5",
-    "Usługa 6",
+    {
+      id: "a",
+      offer: "Usługa 1",
+      isNew: true,
+    },
+    {
+      id: "b",
+      offer: "Usługa 2",
+      isNew: false,
+    },
+    {
+      id: "c",
+      offer: "Usługa 3",
+      isNew: false,
+    },
+    {
+      id: "d",
+      offer: "Usługa 4",
+      isNew: false,
+    },
+    {
+      id: "e",
+      offer: "Usługa 5",
+      isNew: false,
+    },
+    {
+      id: "f",
+      offer: "Usługa 6",
+      isNew: false,
+    },
   ];
-  const listItems = offers.map((offers) => <>{offers}</>);
+  const listItems = offers.map((offers) => (
+    <>
+      <Box key={offers.id} value={offers.offer} isNew={offers.isNew} />
+    </>
+  ));
   return (
     <div className="offer mainpadding" id="offer">
       <div className="main">
         <div>
           <h3>Czym zajmuje się nasza firma?</h3>
         </div>
-        <div className="container boxpadding center">
-          <Box value={listItems[0]} isNew={true} />
-          <Box value={listItems[1]} />
-          <Box value={listItems[2]} />
-        </div>
-        <div className="container boxpadding center">
-          <Box value={listItems[3]} />
-          <Box value={listItems[4]} />
-          <Box value={listItems[5]} />
-        </div>
+        <div className="container boxpadding center">{listItems}</div>
       </div>
     </div>
   );
